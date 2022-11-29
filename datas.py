8c33272b3d2ac50d9616eec9ea0874d82064b51b
REI <51508303+rei-0131@users.noreply.github.com>
2022-11-30 02:36:51 +0900

    Add files via upload

---
+++ datas.py
@@ -0,0 +1,10 @@
+API_key = "TwitterAPI_key"
+API_key_Secret = "TwitterAPI_key_Secret"
+Access_Token = "TwitterAccess_Token"
+Access_Token_Secret = "TwitterAccess_Token_Secret"
+Bearer_Token = "TwitterBearer_Token"
+api_url='https://notify-api.line.me/api/notify'
+search_url="https://api.twitter.com/2/tweets/search/recent"
+test_server='LINE notify トークン'
+path = r"臨時保存先(C:\\Users...)"
+pathtxt = r"臨時保存先(C:\\Users...)"
